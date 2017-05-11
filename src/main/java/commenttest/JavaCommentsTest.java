ee326d0c71302905a8ea4e769292c9faddacc6be
RomanNikitenko <usertaei@some.mail>
2017-05-11 13:38:59 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1493388115633
+//first_change1494509762044
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
